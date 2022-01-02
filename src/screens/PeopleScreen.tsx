1bad615317a9a911fa2f4d3d5d06ebc400713d5f
akshaypatel99 <akshaypatel99@gmail.com>
2022-01-02 18:43:58 +0000

    Display names in Flatlist

--- src/screens/PeopleScreen.tsx
+++ src/screens/PeopleScreen.tsx
@@ -1,8 +1,18 @@
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import { gql, useQuery } from '@apollo/client';
 
 type Person = {
+	id: string;
 	name: string;
+	gender: string;
+	height: number;
+	eyeColor: string;
+	homeworld: {
+		name: string;
+	};
+	species: {
+		name: string;
+	};
 };
 
 interface PeopleData {
@@ -15,7 +25,17 @@ const PEOPLE_QUERY = gql`
 	query People {
 		allPeople {
 			people {
+				id
 				name
+				gender
+				height
+				eyeColor
+				homeworld {
+					name
+				}
+				species {
+					name
+				}
 			}
 		}
 	}
@@ -33,12 +53,21 @@ const PeopleScreen = () => {
 	}
 
 	if (data) {
-		console.log(data.allPeople);
+		console.log(data.allPeople.people);
 	}
 
 	return (
 		<View>
 			<Text></Text>
+			<FlatList
+				data={data?.allPeople.people}
+				renderItem={({ item }) => (
+					<View>
+						<Text>{item.name}</Text>
+					</View>
+				)}
+				keyExtractor={(item) => item.id}
+			/>
 		</View>
 	);
 };
