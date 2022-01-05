293ff7c9c3c687653807677cca0345da7862c406
akshaypatel99 <akshaypatel99@gmail.com>
2022-01-05 21:17:32 +0000

    Add AppLoading, Splash Screen, load fonts in App

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
+import { useEffect, useState } from 'react';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { relayStylePagination } from '@apollo/client/utilities';
 import { NavigationContainer } from '@react-navigation/native';
-
+import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 import BottomTabsNavigator from './src/navigation/BottomTabsNavigator';
 
 // Initialize Apollo Client
@@ -21,6 +24,31 @@ const client = new ApolloClient({
 });
 
 export default function App() {
+	const [isReady, setIsReady] = useState(false);
+
+	async function prepare() {
+		await SplashScreen.preventAutoHideAsync();
+		await Font.loadAsync({
+			SFDistantGalaxy: require('./assets/fonts/SFDistantGalaxy.ttf'),
+			FranklinGothic: require('./assets/fonts/FranklinGothic.ttf'),
+		});
+		setIsReady(true);
+	}
+
+	useEffect(() => {
+		prepare();
+	}, []);
+
+	if (!isReady) {
+		return <AppLoading />;
+	}
+
+	if (isReady) {
+		async () => {
+			await SplashScreen.hideAsync();
+		};
+	}
+
 	return (
 		<ApolloProvider client={client}>
 			<NavigationContainer>
--- src/components/AppText.tsx
+++ src/components/AppText.tsx
@@ -1,8 +1,6 @@
 import { ReactNode } from 'react';
 import { Text } from 'react-native';
-import { useFonts } from 'expo-font';
 import defaultStyles from '../config/styles';
-import colors from '../config/colors';
 
 type AppTextProps = {
 	children: ReactNode;
@@ -17,22 +15,12 @@ const AppText: React.FC<AppTextProps> = ({
 	heading = false,
 	...otherProps
 }) => {
-	const [SFloaded] = useFonts({
-		SFDistantGalaxy: require('../../assets/fonts/SFDistantGalaxy.ttf'),
-	});
-	const [FGloaded] = useFonts({
-		FranklinGothic: require('../../assets/fonts/FranklinGothic.ttf'),
-	});
-
-	let headingStyle =
-		heading && SFloaded
-			? {
-					fontFamily: 'SFDistantGalaxy',
-					textAlign: 'center',
-			  }
-			: FGloaded
-			? { fontFamily: 'FranklinGothic' }
-			: defaultStyles.font;
+	let headingStyle = heading
+		? {
+				fontFamily: 'SFDistantGalaxy',
+				textAlign: 'center',
+		  }
+		: { fontFamily: 'FranklinGothic' };
 
 	return (
 		<Text style={[defaultStyles.text, headingStyle, style]} {...otherProps}>
